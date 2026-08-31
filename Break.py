b79bb419c4a6f2059105b035f3f94ac99eb1abdf
M Umar Adnan <umaradnan2710@gmail.com>
2026-08-31 17:37:49 +0500

    Initial commit

---
+++ Break.py
@@ -0,0 +1,13 @@
+# This program demonstrates the use of the break statement in a for loop
+for i in range(0, 11):
+    if i ==6:
+        break
+    print(i)
+
+# This program demonstrates the use of the break statement in a while loop
+i = 0
+while i < 10:
+    if i == 6:
+        break
+    print(i)
+    i += 1
