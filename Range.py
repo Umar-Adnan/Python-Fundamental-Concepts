399b7c179e2a03e560d5d812f485a4897d8364b4
M Umar Adnan <umaradnan2710@gmail.com>
2026-08-31 14:28:18 +0500

    Initial Commit

--- Range.py
+++ Range.py
@@ -1,3 +1,6 @@
 num = range(1, 11, 2) # Create a range object with values from 1 to 10 (exclusive) with a step of 2
 
 print("The range object:", num)
+print("The values in the range are:")
+for i in num:
+    print(i)
